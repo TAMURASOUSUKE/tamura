6a34cd49f3fcfb7257ef69abb1339b4c302911c1
TAMURASOUSUKE <kokotomo.88934@gmail.com>
2025-11-05 16:29:25 +0900

    プロジェクトの変更

--- 20251105/TaskD.cpp
+++ 20251105/TaskD.cpp
@@ -1,17 +1,8 @@
 #include <iostream>
 
-void FizzBuzz();
-
 const int LOOP_SIZE = 100;
-const int DATA_SIZE = 10;
 
 int main(){
-    FizzBuzz();
-
-    return 0;
-}
-
-void FizzBuzz(){
     for(int i = 1; i <= LOOP_SIZE; ++i){
         bool isFizz = i % 3 == 0;
         bool isBuzz = i % 5 == 0;
@@ -23,4 +14,5 @@ void FizzBuzz(){
     }
 
     std::cout << std::endl;
+    return 0;
 }
