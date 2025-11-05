88affe3688827b961ca441f3e88db2ea947805df
TAMURASOUSUKE <kokotomo.88934@gmail.com>
2025-11-05 15:56:16 +0900

    TaskBの作成

---
+++ 20251105/TaskB.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main(){
+    int i = 1;
+    while (i <= 10)
+    {
+        std::cout << i << "\n";
+        i = i + 1;
+    }
+
+    std::cout << std::endl;
+
+    return 0;
+}
