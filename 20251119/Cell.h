65764b9f2b3b341d431533912d564f3d7a94574b
TAMURASOUSUKE <kokotomo.88934@gmail.com>
2025-11-26 21:42:56 +0900

    20251119のDFSの修正

--- 20251119/Cell.h
+++ 20251119/Cell.h
@@ -6,18 +6,18 @@ struct Cell
     int x{0}, y{0};
 
     
-// 演算子のオーバーロード
-// Cellどうしの加算
-Cell operator +(const Cell& other) const;
-//  Cellどうしの減算
-Cell operator -(const Cell& other) const;
-// Cellどうしの加算代入
-Cell& operator +=(const Cell& other);
-// Cellどうしの減算代入
-Cell& operator -=(const Cell& other);
-// Cellどうしの等価比較
-bool operator ==(const Cell& other) const;
-// Cellどうしの非等価比較
-bool operator !=(const Cell& other) const;
+    // 演算子のオーバーロード
+    // Cellどうしの加算
+    Cell operator +(const Cell& other) const;
+    //  Cellどうしの減算
+    Cell operator -(const Cell& other) const;
+    // Cellどうしの加算代入
+    Cell& operator +=(const Cell& other);
+    // Cellどうしの減算代入
+    Cell& operator -=(const Cell& other);
+    // Cellどうしの等価比較
+    bool operator ==(const Cell& other) const;
+    // Cellどうしの非等価比較
+    bool operator !=(const Cell& other) const;
 };
 
